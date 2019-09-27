66f9cf3a0d57639d09669a474d5ff475c56b7d5e
Laszlo Sandor <voidlaszlo@gmail.com>
2019-09-27 13:50:11 +0200

    adding javascript but just for fun - dont bother

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+// ONLY ADDED THIS BECAUSE WHY NOT IT WORKS WITHOUT THIS TOO ! :) I JUST WANTED TO DO SOMETHING
+const cards = document.querySelectorAll('.card')
+
+openSomething(cards, "/article.html")
+
+function openSomething(arr, url) {
+
+    for( item of arr) {
+        
+        item.addEventListener('click', (e) => {
+            const win = window.open(url, '_blank')
+            win.focus()
+        })
+    }
+}
